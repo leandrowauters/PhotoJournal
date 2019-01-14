dee3495419ae3191146413b6412451ec973c894c
John Doe <leandro.wauters@gmail.com>
2019-01-14 17:16:24 -0500

    first commit

---
+++ NewPhotoJournal/PhotoJournalViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewPhotoJournal
+//
+//  Created by Leandro Wauters on 1/14/19.
+//  Copyright © 2019 Leandro Wauters. All rights reserved.
+//
+
+import UIKit
+
+class PhotoJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
